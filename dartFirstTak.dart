464b036238fb9a46669b278bf33131e3fc400f4b
ROPAHH <ropahmusha15@outlook.com>
2023-04-19 20:32:14 +0200

    First Task

---
+++ dartFirstTak.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  String firstName = "Ropafadzo";
+
+  String lastName = "Mushawembirimi";
+
+  DateTime currentTime = DateTime.now();
+
+  print('Hello, ${firstName} ${lastName}, \n${currentTime}');
+}
